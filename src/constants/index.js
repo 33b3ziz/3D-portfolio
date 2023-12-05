48cdbd556a8b70f3c8f933d9e868cc3d34ff248b
33b3ziz <egyptianhack325@gmail.com>
2023-12-05 10:07:31 +0200

    added experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,6 +22,7 @@ import {
   worldwise,
   threejs,
   creator,
+  tesla,
 } from "../assets";
 
 export const navLinks = [
@@ -134,6 +135,19 @@ const experiences = [
     icon: shopify,
     iconBg: "#383E56",
     date: "Oct 2022 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    company_name: "Tesla",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Jan 2021 - Feb 2022",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
